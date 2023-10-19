a79d5555f57cd87b04e567cad33c60aa7ae38496
Andi-IM <andi.irhamm@gmail.com>
2023-10-19 07:05:51 +0700

    adding project

---
+++ views_and_compose/basic-android-kotlin-compose-training-juice-tracker-compose-starter/app/src/main/java/com/example/juicetracker/ui/homescreen/AdBanner.kt
@@ -0,0 +1,28 @@
+package com.example.juicetracker.ui.homescreen
+
+import android.widget.AdapterView
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.juicetracker.R
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+
+@Composable
+fun AdBanner(modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            AdView(context).apply {
+                setAdSize(AdSize.BANNER)
+                // Use test ad unit ID
+                adUnitId = "ca-app-pub-3940256099942544/6300978111"
+            }
+        }, update = { adView ->
+            adView.loadAd(AdRequest.Builder().build())
+        })
+}
